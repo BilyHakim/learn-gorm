5e499200f705e1f4b2cd3015cdf25efa7a28e7fa
Bily H E <bilyhakim@uca.ac.id>
2023-10-16 11:20:55 +0700

    create user data

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "learn-gorm/database"
+import (
+	"fmt"
+	"learn-gorm/database"
+	"learn-gorm/models"
+)
 
 func main() {
 	database.StartDB()
+
+	createUser("bilyhakim12@gmail.com")
+}
+
+func createUser(email string) {
+	db := database.GetDB()
+
+	User := models.User{
+		Email: email,
+	}
+
+	err := db.Create(&User).Error
+
+	if err != nil {
+		fmt.Println("Error creating user data:", err)
+		return
+	}
+
+	fmt.Println("New User Data:", User)
 }
